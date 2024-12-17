f6b5eee9509e5c21ea303dd4d25ebb1a5ea9e450
filippob <filippo.biscarini@gmail.com>
2024-12-17 17:28:43 +0100

    added: more parameters and controls

--- preprocess_scripts/1.filter_genotypes.sh
+++ preprocess_scripts/1.filter_genotypes.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-plink="~/Downloads/plink"
-basefolder="/home/diazjr/filippo/Montana"
-inputfile="geno_montana_3808_51686" ## binary ped files
+plink="/home/filippo/Downloads/plink"
+basefolder="/home/filippo/Documents/ciampolini/golden_retriever"
+inputfile="dati/plink_files/golden" ## binary ped files
 outputfolder="results"
 max_snp_miss_rate=0.05
+min_maf=0.05
+species=dog
+option=autosome-xy
+
+if [[ ! -e ${basefolder}/${outputfolder} ]]; then
+    mkdir ${basefolder}/${outputfolder}
+fi
+
+basefnm=${inputfile##*/}
+echo "File name is $basefnm"
 
 echo "filtering for SNP missing rate"
-$plink --cow --bfile ${basefolder}/${inputfile} --geno $max_snp_miss_rate --make-bed --out "${basefolder}/${outputfolder}/montana_filtered"
+$plink --$species --bfile ${basefolder}/${inputfile} --geno $max_snp_miss_rate --$option --snps-only --make-bed --out "${basefolder}/${outputfolder}/${basefnm}_filtered"
 
 echo "filtering for sample missing rate"
-$plink --cow --bfile ${basefolder}/${outputfolder}/montana_filtered --geno $max_snp_miss_rate --make-bed --out "${basefolder}/${outputfolder}/montana_filtered"
+$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --mind $max_snp_miss_rate --make-bed --out "${basefolder}/${outputfolder}/${basefnm}_filtered"
 
+## MAF filtering: not needed for ROH analysis !!
 echo "filtering for MAF"
-# plink --cow --bfile geno_montana_3808_51686_geno05 --mind 0.05 --make-bed --out geno_montana_3808_51686_geno05_min05
+#$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --maf $min_maf --make-bed --out ${basefolder}/${outputfolder}/${basefnm}_filtered
 
 # 1.4 Minor allele frequency ----------------------------------------------
-
-# plink --cow --bfile geno_montana_3808_51686_geno05_min05 --freq –out geno_montana_3808_51686_geno05_min05_frq
+$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --freq --missing --out ${basefolder}/${outputfolder}/${basefnm}_filtered
 
 echo "DONE!!"

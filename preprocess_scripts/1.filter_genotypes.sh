afc034dafca7b2168ec9fd3f3778414fa8389f9f
filippob <filippo.biscarini@gmail.com>
2024-12-18 11:57:47 +0100

    added: threshold for per-sample missing rate

--- preprocess_scripts/1.filter_genotypes.sh
+++ preprocess_scripts/1.filter_genotypes.sh
@@ -5,6 +5,7 @@ basefolder="/home/filippo/Documents/ciampolini/golden_retriever"
 inputfile="dati/plink_files/golden" ## binary ped files
 outputfolder="results"
 max_snp_miss_rate=0.05
+max_ind_miss_rate=0.10
 min_maf=
 species=dog
 option=autosome-xy
@@ -16,24 +17,34 @@ fi
 basefnm=${inputfile##*/}
 echo "File name is $basefnm"
 
-echo "Calculating missing rate before filtering:"
+echo "##########################################"
+echo " 1) Calculating missing rate before filtering:"
+echo "##########################################"
 $plink --$species --bfile ${basefolder}/${inputfile} --missing --out ${basefolder}/${outputfolder}/${basefnm}
 
-echo "filtering for SNP missing rate"
+echo "##########################################"
+echo " 2) filtering for SNP missing rate"
+echo "##########################################"
 $plink --$species --bfile ${basefolder}/${inputfile} --geno $max_snp_miss_rate --$option --snps-only --make-bed --out "${basefolder}/${outputfolder}/${basefnm}_filtered"
 
-echo "filtering for sample missing rate"
-$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --mind $max_snp_miss_rate --make-bed --out "${basefolder}/${outputfolder}/${basefnm}_filtered"
+echo "##########################################"
+echo " 3) filtering for sample missing rate"
+echo "##########################################"
+$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --mind $max_ind_miss_rate --make-bed --out "${basefolder}/${outputfolder}/${basefnm}_filtered"
 
 ## MAF filtering: not needed for ROH analysis !!
 
 if [ ! -z "${min_maf}" ]; then
 
-	echo "filtering for MAF"
+	echo "##########################################"
+	echo " 3.5) filtering for MAF"
+	echo "##########################################"
 	$plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --maf $min_maf --make-bed --out ${basefolder}/${outputfolder}/${basefnm}_filtered
 fi
 
-echo "Calculating mssing rate after filtering:"
+echo "##########################################"
+echo " 4) Calculating mssing rate after filtering:"
+echo "##########################################"
 $plink --$species --bfile ${basefolder}/${outputfolder}/${basefnm}_filtered --freq --missing --out ${basefolder}/${outputfolder}/${basefnm}_filtered
 
 echo "DONE!!"
